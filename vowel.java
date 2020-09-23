eb14d0425011ffb84cd74e65e00ffcec60567922
Ankita Yadav <ankitacoder2004@gmail.com>
2020-09-23 17:53:36 +0530

    vowel program done

--- vowel.java
+++ vowel.java
@@ -1,9 +1,18 @@
 import java.util.*;
 
 class vowel {
-    public static void main(String args[])
-    {
-        Scanner sc new Scanner(System.in);
-        char ch = 
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+
+        if (Character.isLetter(ch)) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                System.out.println("The Letter is a Vowel.");
+            } else {
+                System.out.println("Tehe Letter is a Consonant.");
+            }
+        } else {
+            System.out.println("Character not a Letter.");
+        }
     }
 }
