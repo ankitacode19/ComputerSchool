7d23b8cd3d7e9d2eced9e5890f9ef8f7de208164
Ankita Yadav <ankitacoder2004@gmail.com>
2020-09-23 19:04:49 +0530

    Square program done

--- checkSquare.java
+++ checkSquare.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Length");
+        System.out.println("Enter Length:");
         int length = sc.nextInt();
+        System.out.println("Enter Breadth:")
         int breadth = sc.nextInt();
 
         if (length == breadth) {
