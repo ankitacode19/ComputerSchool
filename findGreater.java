8bcf2350fa5b23cdbcc79f496e779decd01bd5c3
Ankita Yadav <ankitacoder2004@gmail.com>
2020-09-23 20:02:36 +0530

    Find greater program done

---
+++ findGreater.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class greater {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number:");
+        int n = sc.nextInt();
+        System.out.println("Enter Second Number:");
+        int m = sc.nextInt();
+
+        if (n > m) {
+            System.out.println(n + " is Greater.");
+        } else if (m > n) {
+            System.out.println(m + " is Greater.");
+        } else {
+            System.out.println(m + " and " + n + " are equal.");
+        }
+    }
+}
