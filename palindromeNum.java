014638d6793fe97d52319b3a1d2d439c99c26668
Ankita Yadav <ankitacoder2004@gmail.com>
2020-09-29 21:37:06 +0530

    wrong palindrome

---
+++ palindromeNum.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class CheckPalindrome {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number to check Palindrome:");
+        int n = sc.nextInt();
+        int temp = n;
+        int d = 0, c;
+
+        while (n > 0) {
+            c = n % 10;
+            d = (c * 100) + d;
+            n = n / 10;
+        }
+
+        if (d == temp) {
+            System.out.println(temp + " is a palindrome number.");
+        } else {
+            System.out.println(temp + " is not a palindrome number.");
+        }
+        sc.close();
+    }
+}
